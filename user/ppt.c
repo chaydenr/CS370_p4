28917f2f60e386de9b827f8c159aefdcaa71d8bd
chaydenr <75277857+chaydenr@users.noreply.github.com>
2023-06-29 21:31:26 -0700

    track user/ppt.c

---
+++ user/ppt.c
@@ -0,0 +1,13 @@
+#include "user.h"
+#include "usys.pl"
+#include "kernel/syscall.h"
+#include "kernel/sysproc.c"
+#include "kernel/defs.h"
+#include "kernel/vm.c"
+
+int main(int argc, char *argv[]) 
+{
+    printf("Hello world (from Chayden)");
+
+    return 0;
+};
